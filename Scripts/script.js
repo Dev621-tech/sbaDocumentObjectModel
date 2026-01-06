937af65a22de4d77836ad6999155cf91e91ef6b1
Dev621-tech <devinosborne11@yahoo.com>
2026-01-06 17:39:51 -0500

    Title Done

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+// MAIN TITLE "ACCOMPLISHMENT TRACKER"
+let title = document.getElementsByTagName('header');
+title[0].style.backgroundColor = 'black'
+let brandname = document.createElement('h1');
+brandname.textContent = 'Accomplishment Tracker'
+title[0].append(brandname);
+
