f5ded70dea76c711803b1ac50265fa884b45c9fd
Dev621-tech <devinosborne11@yahoo.com>
2026-01-06 18:06:02 -0500

    BETTER TITLE

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,12 @@
+document.body.style.margin = '0%';
+
 // MAIN TITLE "ACCOMPLISHMENT TRACKER"
 let title = document.getElementsByTagName('header');
-title[0].style.backgroundColor = 'black'
+title[0].style.backgroundColor = 'lightblue';
 let brandname = document.createElement('h1');
 brandname.textContent = 'Accomplishment Tracker'
 title[0].append(brandname);
+brandname.style.margin = '0';
+brandname.style.fontSize = "5rem";
 
+// NAVIGATION BAR
