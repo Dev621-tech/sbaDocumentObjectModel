1a8eb30058f20b20988b355bb78384b29770704f
Dev621-tech <devinosborne11@yahoo.com>
2026-01-06 23:00:33 -0500

    LANDING PAGE

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## [SBA 316: THE DOCUMENT OBJECT MODEL](https://www.canva.com/design/DAFrivz5SU8/view?embed)
+
+
+
+[Hard-Working Man Picture](https://www.pexels.com/photo/a-man-sitting-at-the-table-6282069/)
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,26 +1,29 @@
 document.body.style.margin = '0%';
 
-// MAIN TITLE "ACCOMPLISHMENT TRACKER"
+// MAIN TITLE "ACCOMPLISHMENT TRACKER" -----------------------------------------------
 let title = document.getElementsByTagName('header');
 title[0].style.backgroundColor = 'lightblue';
 let brandname = document.createElement('h1');
 brandname.textContent = 'Accomplishment Tracker'
 title[0].append(brandname);
+
+
 brandname.style.margin = '0';
 brandname.style.fontSize = "5rem";
+brandname.style.fontFamily = "'Courier New', monospace";
 
-// NAVIGATION BAR
+// NAVIGATION BAR -----------------------------------------------------------------------
 const nav = document.createElement('nav');
 
 const navLinks = [
-    {text: 'Home', href: '/home'}, 
-    {text: 'Create', href: '/create'}, 
-    {text: 'ToDo', href: '/todo'},
-    {text: 'Progress', href: '/progress'},
-    {text: 'Profile', href: '/profile'}
+    { text: 'Home', href: 'index.html' },
+    { text: 'Create', href: '/create' },
+    { text: 'ToDo', href: '/todo' },
+    { text: 'Progress', href: '/progress' },
+    { text: 'Profile', href: '/profile' }
 ];
 
-navLinks.forEach(link =>{
+navLinks.forEach(link => {
     let a = document.createElement('a');
     a.textContent = link.text;
     a.href = link.href;
@@ -29,6 +32,36 @@ navLinks.forEach(link =>{
 
 });
 
-title[0].appendChild(nav);
-
+document.body.appendChild(nav);
 nav.classList.add('flex-nav');
+
+// MAIN CONTAINER --------------------------------------------------------------------
+let main = document.querySelector('main');
+
+// Ensures that the nav is before the main
+document.body.insertBefore(nav, main);
+
+let container = document.createElement('div');
+container.classList.add('image-text-container');
+
+// IMAGE
+const img = document.createElement('img');
+img.src = "images/hardWorking.jpg"
+img.alt = "Young Man Working Hard"
+img.classList.add('side-image');
+
+// TEXT BOX
+const textBox = document.createElement('div');
+textBox.classList.add('text-box');
+textBox.innerHTML = `<h2>Where Everything Is An Accomplishment!</h2>
+<p>IT MAY NOT BE AN ACCOMPLISHMENT TO THEM, BUT IT WAS AN ACCOMPLSIHMENT TO YOU !!</p>`
+    ;
+
+
+container.appendChild(img);
+container.appendChild(textBox);
+
+main.appendChild(container);
+
+
+// Seconf
