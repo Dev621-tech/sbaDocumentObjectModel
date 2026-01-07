2d80599b35c2b070cb6a9e4e982dbef9157306f8
Dev621-tech <devinosborne11@yahoo.com>
2026-01-06 21:17:18 -0500

    Nav Bar

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,3 +10,25 @@ brandname.style.margin = '0';
 brandname.style.fontSize = "5rem";
 
 // NAVIGATION BAR
+const nav = document.createElement('nav');
+
+const navLinks = [
+    {text: 'Home', href: '/home'}, 
+    {text: 'Create', href: '/create'}, 
+    {text: 'ToDo', href: '/todo'},
+    {text: 'Progress', href: '/progress'},
+    {text: 'Profile', href: '/profile'}
+];
+
+navLinks.forEach(link =>{
+    let a = document.createElement('a');
+    a.textContent = link.text;
+    a.href = link.href;
+
+    nav.appendChild(a);
+
+});
+
+title[0].appendChild(nav);
+
+nav.classList.add('flex-nav');
